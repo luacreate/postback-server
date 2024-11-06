d3c22ee706a5a35e25c3bad9cd77df2e0bd12f22
luacreate <id392442296@gmail.com>
2024-11-06 14:22:38 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
+from flask import Flask, request, jsonify
 import os
 import json
-from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -15,16 +15,13 @@ if not os.path.exists(DATA_FILE):
 # Обработчик для приема постбэков от 1WIN
 @app.route('/postback-handler', methods=['GET'])
 def postback_handler():
-    # Получаем параметр text из URL
     text = request.args.get('text')
     
     if text:
         try:
-            # Парсим строку text, чтобы получить данные
             print(f"Получено значение text: {text}")
             parts = text.split(':')
 
-            # Проверка на наличие двух частей в строке
             if len(parts) != 2:
                 print("Ошибка: Некорректный формат text")
                 return "Ошибка: Некорректный формат text", 400
@@ -32,21 +29,17 @@ def postback_handler():
             user_id, amount = parts
             amount = float(amount)
 
-            # Чтение текущих данных из файла
             try:
                 with open(DATA_FILE, 'r') as file:
                     data = json.load(file)
 
-                # Проверка, существует ли запись для данного user_id
                 user_found = False
                 for entry in data:
                     if entry['user_id'] == user_id:
-                        # Обновляем сумму депозита
                         entry['amount'] += amount
                         user_found = True
                         break
 
-                # Если запись не найдена, добавляем новую
                 if not user_found:
                     new_entry = {
                         "user_id": user_id,
@@ -54,7 +47,6 @@ def postback_handler():
                     }
                     data.append(new_entry)
 
-                # Запись обновленных данных в файл
                 with open(DATA_FILE, 'w') as file:
                     json.dump(data, file, indent=4)
 
@@ -69,6 +61,17 @@ def postback_handler():
     else:
         print("Параметр text не найден")
         return "Параметр text не найден", 400
+
+# Добавляем новый маршрут для получения данных из data.json
+@app.route('/data', methods=['GET'])
+def get_data():
+    try:
+        with open(DATA_FILE, 'r') as file:
+            data = json.load(file)
+        return jsonify(data)  # Возвращаем данные в формате JSON
+    except Exception as e:
+        print(f"Ошибка при чтении data.json: {e}")
+        return "Ошибка при чтении данных", 500
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
